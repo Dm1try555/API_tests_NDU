07609bbb303e7ca12502377cae5b15ab04c17b58
Dima <shilovscky2020@gmail.com>
2025-02-18 16:08:42 +0200

    Folder document: updated endpoints.py

--- services/document/endpoints.py
+++ services/document/endpoints.py
@@ -1,7 +1,9 @@
+# import os
+# from dotenv import load_dotenv
 #
-#
-# HOST = "https://llc-cabinet-api-mwt.csd.ua/api/v1"
-#
+# load_dotenv()
+# HOST = os.getenv("HOST")
+
 # class Endpoints:
 #
 #     create_document = f"{HOST}/document"
