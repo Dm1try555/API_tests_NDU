a217e088a0bd01dcc17f4393adb96200e2d9bb09
Dima <shilovscky2020@gmail.com>
2025-03-14 09:16:04 +0200

    edit file_model.py: - add model create file model

--- services/file/models/file_model.py
+++ services/file/models/file_model.py
@@ -1,10 +1,24 @@
 from pydantic import BaseModel, field_validator
 
 
+'''Create File'''
+
+class CreateFileData(BaseModel):
+    fileName: str
+    contentType: str
+    fileSize: str
+    url: str
+    @field_validator("fileName", "contentType", "fileSize", "url")
+    def message_is_valid(cls, value):
+        if value == "" or value is None:
+            raise ValueError("Field is empty")
+        else:
+            return value
+
 
 class CreateFileModel(BaseModel):
     message: str
-    data: str
+    data: CreateFileData
     @field_validator("message", "data")
     def message_is_valid(cls, value):
         if value == "" or value is None:
