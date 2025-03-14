3c20b805248aa151c43add9e25356cd2d7fca1c2
Dima <shilovscky2020@gmail.com>
2025-03-14 11:54:55 +0200

    edit test_llc.py: - add pytest.fixture create user and return user_id
    - In class TestLLC: add user_id = None
    - Save the user_id from the fixture
    -Use user_id in tests

--- tests/test_llc.py
+++ tests/test_llc.py
@@ -4,7 +4,6 @@ from services.llc.api_llc import LlcAPI
 from tests.test_user import TestUser
 
 
-
 @pytest.fixture
 def create_user():
     user_test = TestUser()
@@ -18,13 +17,11 @@ def create_user():
 class TestLLC:
 
     id_llc = None
+    user_id = None
 
     @classmethod
     def setup_class(cls):
-
         cls.api_llc = LlcAPI()
-
-
 
     @allure.title("Get llc by filter")
     def test_llc_by_filter(self):
@@ -34,7 +31,6 @@ class TestLLC:
         self.__class__.id_llc = model.data.items[1].id
         print(f"Extracted ID second LLC: {self.__class__.id_llc}")
 
-
     @allure.title("Get llc by ID")
     def test_llc_by_id(self):
         model = self.api_llc.get_llc_by_id(self.__class__.id_llc)
@@ -42,10 +38,10 @@ class TestLLC:
         assert model.data is not None
         assert model.data.id == self.__class__.id_llc
 
-
     @allure.title("Add manager to LLC")
     def test_add_manager_to_llc(self, create_user):
-        model = self.api_llc.add_manager_to_llc(self.__class__.id_llc, create_user)
+        self.__class__.user_id = create_user  # Save the user_id from the fixture
+        model = self.api_llc.add_manager_to_llc(self.__class__.id_llc, self.__class__.user_id)
         assert model.message == "Manager added to llc  successfully"
 
     @allure.title("Check manager after add manager to LLC")
@@ -54,15 +50,22 @@ class TestLLC:
         assert model.message == "LLC retrieved successfully"
         assert model.data is not None
         assert model.data.id == self.__class__.id_llc
-        assert model.data.managers, "Список managers пуст, хотя менеджер должен был быть добавлен."
+        assert model.data.managers, "The managers list is empty, although a manager should have been added."
         for user_ids in model.data.managers:
             print(user_ids)
             assert user_ids is not None
 
     @allure.title("Add members to LLC")
-    def test_add_members_to_llc(self, create_user):
-        model = self.api_llc.add_manager_to_llc(self.__class__.id_llc, create_user)
+    def test_add_members_to_llc(self):
+        # Use the user_id saved from the previous test
+        model = self.api_llc.add_manager_to_llc(self.__class__.id_llc, self.__class__.user_id)
         assert model.message == "Manager added to llc  successfully"
+
+
+
+
+
+
 
     # @allure.title("Check members after add members to LLC")
     # def test_check_after_add_members(self):
