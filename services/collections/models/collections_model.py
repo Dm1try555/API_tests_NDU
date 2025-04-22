d07557dbd164ad9674a2afdd2cf29b7dcf6bf0b5
Dima <shilovscky2020@gmail.com>
2025-04-22 20:31:51 +0300

    add collections tests

--- services/collections/models/collections_model.py
+++ services/collections/models/collections_model.py
@@ -2,7 +2,7 @@ from typing import Optional, Union
 from pydantic import BaseModel, field_validator
 
 
-class CollectionsDocumentStatusModel(BaseModel):
+class DocumentStatusModel(BaseModel):
     key: str
     value: int
     displayName: str
@@ -15,7 +15,7 @@ class CollectionsDocumentStatusModel(BaseModel):
             return value
 
 
-class CollectionsMemberTypeModel(BaseModel):
+class llcMemberTypeModel(BaseModel):
     key: str
     value: int
     displayName: str
@@ -27,12 +27,65 @@ class CollectionsMemberTypeModel(BaseModel):
         else:
             return value
 
-class CollectionsDocumentTypeModel(BaseModel):
+class DocumentTypeModel(BaseModel):
     key: str
     value: int
     displayName: str
 
     @field_validator("key", "value", "displayName")
+    def fields_are_valid(cls, value):
+        if value == "" or value is None:
+            raise ValueError("Field is empty")
+        else:
+            return value
+
+class UserStatusTypeModel(BaseModel):
+    key: str
+    value: int
+    displayName: str
+
+    @field_validator("key", "value", "displayName")
+    def fields_are_valid(cls, value):
+        if value == "" or value is None:
+            raise ValueError("Field is empty")
+        else:
+            return value
+        
+
+class PrintedFormTypeModel(BaseModel):
+    key: str
+    value: int
+    displayName: str
+
+    @field_validator("key", "value", "displayName")
+    def fields_are_valid(cls, value):
+        if value == "" or value is None:
+            raise ValueError("Field is empty")
+        else:
+            return value
+        
+class SignatureTypeModel(BaseModel):
+    key: str
+    value: int
+    displayName: str
+
+    @field_validator("key", "value", "displayName")
+    def fields_are_valid(cls, value):
+        if value == "" or value is None:
+            raise ValueError("Field is empty")
+        else:
+            return value
+        
+
+        
+class DataModel(BaseModel):
+    documentStatus: list[DocumentStatusModel] 
+    llcMemberType: list[llcMemberTypeModel] 
+    documentType: list[DocumentTypeModel] 
+    userStatusType: list[UserStatusTypeModel] 
+    printedFormType: list[PrintedFormTypeModel] 
+    signatureType: list[SignatureTypeModel] 
+    @field_validator("documentStatus", "llcMemberType", "documentType", "userStatusType", "printedFormType", "signatureType")
     def fields_are_valid(cls, value):
         if value == "" or value is None:
             raise ValueError("Field is empty")
@@ -42,8 +95,7 @@ class CollectionsDocumentTypeModel(BaseModel):
 
 class CollectionsModel(BaseModel):
     message: str
-    data: dict[str, list[Union[CollectionsDocumentStatusModel, CollectionsMemberTypeModel, CollectionsDocumentTypeModel]]]
-
+    data: DataModel 
     @field_validator("message", "data")
     def message_is_valid(cls, value):
         if value == "" or value is None:
--- tests/test_collections.py
+++ tests/test_collections.py
@@ -12,5 +12,21 @@ class TestCollections:
 
     @allure.title("Get collections")
     def test_get_collections(self):
-        self.api_collections.get_collections()
+        model = self.api_collections.get_collections()
+        assert model.message == "Дані успішно отримано."
+        data = model.data
+
+        assert data.documentStatus is not None, "Document status is None"
+        assert data.llcMemberType is not None, "LLC member type is None"
+        assert data.documentType is not None, "Document type is None"
+        assert data.userStatusType is not None, "User status type is None"
+        assert data.printedFormType is not None, "Printed form type is None"
+        assert data.signatureType is not None, "Signature type is None"
+
+        assert len(data.documentStatus) > 0, "Document status list is empty"
+        assert len(data.llcMemberType) > 0, "LLC member type list is empty"
+        assert len(data.documentType) > 0, "Document type list is empty"
+        assert len(data.userStatusType) > 0, "User status type list is empty"
+        assert len(data.printedFormType) > 0, "Printed form type list is empty"
+        assert len(data.signatureType) > 0, "Signature type list is empty"
 
